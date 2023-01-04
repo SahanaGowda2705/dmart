c223790b02b442f532f8b9763cb6e9db6730be98
SahanaGowda2705 <121846505+SahanaGowda2705@users.noreply.github.com>
2023-01-04 16:46:53 +0530

    Add files via upload

---
+++ raz.java
@@ -0,0 +1,12 @@
+public class raz {
+    public static void main(String[] args)
+
+    {
+        int a = 6;
+        int c = 9;
+
+        System.out.println("hello world");
+        System.out.println(a + c);
+
+    }
+}
